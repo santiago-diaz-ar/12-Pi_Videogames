6e13e277ae726e903ecda2dd6e6aadb6afc2b57d
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-01-06 01:19:16 -0500

    json para parcear de ejemplo creado

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,29 +1,104 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
+import style from "./Home.module.css";
+
 export default function Home() {
+  const videojuegos = `
+  [
+    {
+      "id": "1",
+      "nombre": "Halo",
+      "imagen": "en desarrollo",
+      "plataformas": "xbox",
+      "descripcion": "Buen juego de balo y extraterrestes",
+      "fecha de lanzamiento": "01/01/2023",
+      "rating": "10.0",
+      "generos": "accion,ficcion"
+    },
+  
+    {
+      "id": "1",
+      "nombre": "Halo",
+      "imagen": "en desarrollo",
+      "plataformas": "xbox",
+      "descripcion": "Buen juego de balo y extraterrestes",
+      "fecha de lanzamiento": "01/01/2023",
+      "rating": "10.0",
+      "generos": "accion,ficcion"
+    },
+  
+    {
+      "id": "1",
+      "nombre": "Halo",
+      "imagen": "en desarrollo",
+      "plataformas": "xbox",
+      "descripcion": "Buen juego de balo y extraterrestes",
+      "fecha de lanzamiento": "01/01/2023",
+      "rating": "10.0",
+      "generos": "accion,ficcion"
+    },
+    {
+      "id": "1",
+      "nombre": "Halo",
+      "imagen": "en desarrollo",
+      "plataformas": "xbox",
+      "descripcion": "Buen juego de balo y extraterrestes",
+      "fecha de lanzamiento": "01/01/2023",
+      "rating": "10.0",
+      "generos": "accion,ficcion"
+    },
+    {
+      "id": "1",
+      "nombre": "Halo",
+      "imagen": "en desarrollo",
+      "plataformas": "xbox",
+      "descripcion": "Buen juego de balo y extraterrestes",
+      "fecha de lanzamiento": "01/01/2023",
+      "rating": "10.0",
+      "generos": "accion,ficcion"
+    },
+    {
+      "id": "1",
+      "nombre": "Halo",
+      "imagen": "en desarrollo",
+      "plataformas": "xbox",
+      "descripcion": "Buen juego de balo y extraterrestes",
+      "fecha de lanzamiento": "01/01/2023",
+      "rating": "10.0",
+      "generos": "accion,ficcion"
+    }
+  ]
+  `;
+
+  useEffect(() => {
+    console.log(typeof videojuegos);
+  });
+
   return (
     <>
-      <br />
-      <br />
-      <button>
-        <NavLink to="/">Volver</NavLink>
-      </button>
-      <br />
-      <br />
-      <br />
-      <br />
-      <div>NavBar en desarrollo</div>
-      <br />
-      <br />
-      <br />
-      <button>
-        <NavLink to="/About">Ir A Sobre mi</NavLink>
-      </button>
-      <br />
-      <br />
-      <br />
-      <h1>Home Se encuentra en desarrollo</h1>
+      <div className={style.padre}>
+        <button /* className={style.muestra} */>
+          <NavLink to="/">Volver</NavLink>
+        </button>
+        <br />
+        <button>
+          <NavLink to="/About">Ir A Sobre mi</NavLink>
+        </button>
+        <br />
+        <div>
+          <input
+            type="text"
+            name="Busquedas"
+            id=""
+            placeholder="Buscar videojuegos"
+          />
+          <button>Buscar</button>
+        </div>
+        <br />
+
+        <div></div>
+      </div>
     </>
   );
 }
